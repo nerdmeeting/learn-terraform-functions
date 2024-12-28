a549a49b14e9985d70c648e8f478e4c07b7c6f0d
Nathan Bills <nathan@nerdmeeting.com>
2024-12-27 18:31:14 -0700

    Update the ami attribute within the aws_instance resource to use the lookup function.
    The ami is a required attribute for the aws_instance resource, so the lookup function must return a valid value for Terraform to apply your configuration. The lookup function arguments are a map, the key to access in the map, and an optional default value in case the key does not exist.

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "aws_security_group" "sg_8080" {
 
 
 resource "aws_instance" "web" {
-  ami                         = data.aws_ami.ubuntu.id
+  ami                         = lookup(var.aws_amis, var.aws_region)
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.subnet_public.id
   vpc_security_group_ids      = [aws_security_group.sg_8080.id]
