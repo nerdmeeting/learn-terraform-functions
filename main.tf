5edc6866eeb64200d121e794e5fa23dd1c2d27f0
Nathan Bills <nathan@nerdmeeting.com>
2024-12-27 18:44:18 -0700

    Add a new aws_security_group resource and aws_key_par to allow SSH access.
    This configuration uses the file function to read the contents of a file to configure an SSH key pair. The file function does not interpolate values into file contents; you should only use it with files that do not need modification.

--- main.tf
+++ main.tf
@@ -67,3 +67,20 @@ resource "aws_instance" "web" {
   associate_public_ip_address = true
   user_data                   = templatefile("user_data.tftpl", { department = var.user_department, name = var.user_name })
 }
+
+resource "aws_security_group" "sg_22" {
+  name = "sg_22"
+  vpc_id = aws_vpc.vpc.id
+  
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name = "ssh_key"
+  public_key = file("ssh_key.pub")
+}
